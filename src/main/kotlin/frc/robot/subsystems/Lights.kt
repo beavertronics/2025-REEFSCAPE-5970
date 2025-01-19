2a835b13248d6d5c7b1227e53afe52133442ecea
Will Kam <william@kamelot.me>
2025-01-18 21:27:12 -0800

    Implement LEDs on PWM channel 0
    Like the driver station once was

---
+++ src/main/kotlin/frc/robot/subsystems/Lights.kt
@@ -0,0 +1,30 @@
+package frc.robot.subsystems
+
+import edu.wpi.first.wpilibj.AddressableLED
+import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import edu.wpi.first.wpilibj.LEDPattern
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.units.Units
+
+object Lights : SubsystemBase() {
+
+    val length = 2 * 60; // 2 meters times 60 LEDs per meter
+    val density = Units.Meters.of(1.0/60);
+
+    val lights = AddressableLED(0); // PWM port 0
+    var buffer = AddressableLEDBuffer(length);
+
+    // Patterns:
+    val scrollingRainbow = LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(1.0),density); //255 = max saturation, 128 = half brightness, scroll at 1 m/s
+
+
+    init {
+        lights.setLength(length); // Length in meters times 60
+        lights.start();
+    }
+
+    override fun periodic() {
+        scrollingRainbow.applyTo(buffer);
+        lights.setData(buffer);
+    }
+}
