28687f25b3f04e4e9a0753a4fbf12a3309b462da
BubbleShade <ozypfl@gmail.com>
2025-01-25 12:01:17 -0800

    Cleaned up drive functions, added sysID capability, added functions to test if the mechanical of the modules work

--- src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
+++ src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
@@ -1,9 +1,20 @@
 package frc.robot.subsystems
 
+import beaverlib.utils.Units.Electrical.VoltageUnit
+import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj.Filesystem
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj2.command.WaitCommand
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine
 import swervelib.SwerveDrive
+import swervelib.SwerveDriveTest
+import swervelib.SwerveModule
 import swervelib.parser.SwerveParser
 import swervelib.telemetry.SwerveDriveTelemetry
 import swervelib.telemetry.SwerveDriveTelemetry.*
@@ -26,12 +37,6 @@ object Drivetrain : SubsystemBase() {
     // create anything that is set later (late init)
     var swerveDrive: SwerveDrive
 
-    // variables created now
-    private var fieldOriented: Boolean = true
-
-    // anything to get values
-    val isFieldOriented get() = fieldOriented
-
     /**
      * init file that runs on intialization of drivetrain class
      */
@@ -40,27 +45,92 @@ object Drivetrain : SubsystemBase() {
         SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH
         swerveDrive = SwerveParser(DriveConstants.DriveConfig).createSwerveDrive(DriveConstants.MaxSpeed)
     }
-
-    fun makeChassisSpeed(xSpeedMPS: Double, ySpeedMPS: Double, rotationOmegaRPS: Double): ChassisSpeeds {
-        return ChassisSpeeds(xSpeedMPS, ySpeedMPS, rotationOmegaRPS)
+    /**
+     * Directly send voltage to the drive motors.
+     * @param volts The voltage to send to the motors.
+     */
+    fun setDriveMotorVoltageRaw(volts: VoltageUnit){
+        swerveDrive.modules.forEach {
+            it.driveMotor.voltage = volts.asVolts
+        }
+    }
+    /**
+     * Directly send voltage to the angle motors.
+     * @param volts The voltage to send to the motors.
+     */
+    fun setAngleMotorVoltageRaw(volts: VoltageUnit){
+        swerveDrive.modules.forEach {
+            it.angleMotor.voltage = volts.asVolts
+        }
     }
 
     /**
-     * sets field oriented to inputted state
+     * Advanced drive method that translates and rotates the robot, with a custom center of rotation.
+     * @param translation The desired X and Y velocity of the robot.
+     * @param rotation The desired rotational velocity of the robot.
+     * @param fieldOriented Whether the robot's motion should be field oriented or robot oriented.
+     * @param centerOfRotation The center of rotation of the robot.
      */
-    fun setFieldOriented(state: Boolean) { fieldOriented = state }
+    fun drive(
+        translation: Translation2d,
+        rotation: Double = 0.0,
+        fieldOriented: Boolean = false,
+        centerOfRotation: Translation2d = Translation2d()
+    ) {
+        swerveDrive.drive(translation, rotation, fieldOriented, false, centerOfRotation)
+    }
+    /**
+     * Advanced drive method that translates and rotates the robot, with a custom center of rotation.
+     * @param translation The desired X and Y velocity of the robot.
+     * @param rotation The desired rotational velocity of the robot.
+     * @param fieldOriented Whether the robot's motion should be field oriented or robot oriented.
+     * @param centerOfRotation The center of rotation of the robot.
+     */
+    fun driveOpenLoop(
+        translation: Translation2d,
+        rotation: Double = 0.0,
+        fieldOriented: Boolean = false,
+        centerOfRotation: Translation2d = Translation2d()
+    ) {
+        swerveDrive.drive(translation, rotation, fieldOriented, true, centerOfRotation)
+    }
 
     /**
-     * drives the robot field oriented.
-     * this means that no matter the robots rotation, driving "forwards" on the joysticks
-     * makes the robot go forwards on the field
+     * Simple drive method that uses ChassisSpeeds to control the robot.
+     * @param velocity The desired ChassisSpeeds of the robot
+     * @param fieldOriented if false, drive the robot such that forwards is in the direction the robot is facing
+     * if true, forward will be forward relative to the field.
      */
-    fun driveFieldOriented(speed: ChassisSpeeds) { swerveDrive.driveFieldOriented(speed) }
+    fun drive(velocity: ChassisSpeeds, fieldOriented: Boolean = false) {
+        if(fieldOriented) swerveDrive.driveFieldOriented(velocity); else swerveDrive.drive(velocity)
+    }
 
     /**
-     * drives the robot regardless of field orientation.
-     * this means that driving forwards just makes the robot go in the direction
-     * of the front of the robot
+     * Return SysID command for drive motors from YAGSL
+     * @return A command that SysIDs the drive motors.
      */
-    fun drive(speed: ChassisSpeeds) { swerveDrive.drive(speed) }
+    fun sysIdDriveMotor(): Command? {
+        return SwerveDriveTest.generateSysIdCommand(
+            SwerveDriveTest.setDriveSysIdRoutine(
+                SysIdRoutine.Config(),
+                this,
+                swerveDrive, 12.0),
+            3.0, 5.0, 3.0
+        )
+    }
+
+    /**
+     * Return SysID command for angle motors from YAGSL
+     * @return A command that SysIDs the angle1 motors.
+     */
+    fun sysIdAngleMotorCommand(): Command {
+        return SwerveDriveTest.generateSysIdCommand(
+            SwerveDriveTest.setAngleSysIdRoutine(
+                SysIdRoutine.Config(),
+                this, swerveDrive
+            ),
+            3.0, 5.0, 3.0
+        )
+    }
+
 }
