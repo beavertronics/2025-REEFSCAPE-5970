b90b47b00d359a3ac4ed96f18701a6331fd0d9af
Will Kam <william@kamelot.me>
2025-01-27 17:35:52 -0800

    Worky

--- src/main/kotlin/frc/robot/subsystems/Lights.kt
+++ src/main/kotlin/frc/robot/subsystems/Lights.kt
@@ -1,14 +1,25 @@
 package frc.robot.subsystems
 
+import edu.wpi.first.units.DimensionlessUnit
 import edu.wpi.first.wpilibj.AddressableLED
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
 import edu.wpi.first.wpilibj.LEDPattern
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.units.Units
+import edu.wpi.first.units.Units.Percent
+import edu.wpi.first.units.measure.Dimensionless
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj.util.Color
+import edu.wpi.first.wpilibj2.command.Command
+import jdk.jfr.Percentage
+import kotlin.math.round
+import kotlin.math.roundToInt
 
+fun beaverColor(red : Int, green : Int, blue : Int, alpha : Double = 1.0) : Color {
+    return Color((green * alpha).roundToInt(), (red * alpha).roundToInt(), round(blue * alpha).roundToInt())
+}
 object Lights : SubsystemBase() {
-
+    val timer = Timer()
     val length = 5 * 60; // 2 meters times 60 LEDs per meter
     val density = Units.Meters.of(1.0/60);
 
@@ -17,21 +28,40 @@ object Lights : SubsystemBase() {
 
     // Patterns:
     val scrollingRainbow = LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(1.0),density); //255 = max saturation, 128 = half brightness, scroll at 1 m/s
-    val Transflag = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous,
-        Color(91, 206, 250), //Transblue
-        Color(245, 169, 184), //Transpink
-        Color(255,255,255), //White
-        Color(245, 169, 184), //Transpink
-        Color(91, 206, 250)) //Transblue
+    val Transflag = LEDPattern.steps(
+        mutableMapOf(
+            Pair(0, beaverColor(45, 102, 250)), //blue
+            Pair(0.2, beaverColor(245, 85, 92)), //blue
+            Pair(0.4, beaverColor(255,255,255)), //blue
+            Pair(0.6, beaverColor(245, 85, 92)), //blue
+            Pair(0.8, beaverColor(45, 102, 250))) //blue
+    ).scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(0.5),density).atBrightness(Percent.of(75.0))
+    val AceFlag = LEDPattern.steps(
+        mutableMapOf(
+            Pair(0, beaverColor(0, 0, 250)), //blue
+            Pair(0.2, beaverColor(245, 85, 92)), //blue
+            Pair(0.4, beaverColor(255,255,255)), //blue
+            Pair(0.6, beaverColor(245, 85, 92)), //blue
+            Pair(0.8, beaverColor(45, 102, 250))) //blue
+    ).scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(1.0),density).atBrightness(Percent.of(75.0))
+    init {
+        defaultCommand = runPattern(Transflag).withName("Trans")
+    }
     fun init() {
-        lights.setLength(length); // Length in meters times 60
-        lights.start();
-        Transflag.applyTo(buffer);
+        lights.setLength(length) // Length in meters times 60
+        lights.start()
         lights.setData(buffer);
     }
 
     override fun periodic() {
-        //scrollingRainbow.applyTo(buffer);
-        //lights.setData(buffer);
+
+        //Transflag.applyTo(buffer);
+        lights.setData(buffer);
+        //if(timer.hasElapsed(5.0))
+    }
+
+    fun runPattern(pattern: LEDPattern) : Command {
+        val command : Command = run { pattern.applyTo(buffer) }.repeatedly().ignoringDisable(true)
+        return command
     }
 }
