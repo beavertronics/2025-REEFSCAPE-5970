64aed011234e89cd3ef9b85a6125da8d8d00fcdb
Will Kam <william@kamelot.me>
2025-02-10 17:49:46 -0800

    Pain and suffering

--- src/main/kotlin/frc/robot/subsystems/Sensors.kt
+++ src/main/kotlin/frc/robot/subsystems/Sensors.kt
@@ -1,5 +1,6 @@
 package frc.robot.subsystems
 
+import beaverlib.utils.Sugar.clamp
 import beaverlib.utils.Units.Angular.*
 import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.DutyCycleEncoder
@@ -16,9 +17,16 @@ import kotlin.math.PI
 class BeaverDutyCycle(
     channel: Int,
     var ratio : Double = 1.0,
-    var offset : AngleUnit = 0.0.rotations
+    var offset : AngleUnit = 0.0.rotations,
+    var lowerLimit : AngleUnit = 0.0.radians,
+    var upperLimit : AngleUnit = 360.degrees
 ) : DutyCycleEncoder(channel, 2* PI, 0.0) {
-    val rotations : AngleUnit get() = (this.get().radians + (offset/ratio)) * ratio
+    val rotations : AngleUnit get() =
+        ((this.get().radians + (offset/ratio)) * ratio)//.asRadians
+            //.clamp(lowerLimit.asRadians, upperLimit.asRadians).radians
+    fun reset(newValue : AngleUnit) {
+        offset = (offset - rotations) + newValue
+    }
 
         //(this.get().radians * ratio) + offset
 }
@@ -26,23 +34,23 @@ class BeaverDutyCycle(
 inline val AngleUnit.standardPosition get() =
     if(this.asRadians < 0.0) { ( AngleUnit((2*PI) + (this.asRadians % (2 * PI)))) }
 else { AngleUnit(this.asRadians % (2 * PI)) }
+inline val AngleUnit.cook get() = AngleUnit(this.asRadians % (2* PI))
 
 object Sensors : SubsystemBase() {
     // motor to encoder: 30:48
     // encoder to arm: 3:1
 
-    val encoder = DutyCycleEncoder(
+    val encoder = BeaverDutyCycle(
         RobotInfo.ArmThroughboreEncoderDIO,
-        2* PI,
-        0.0
-
+        1.0
     )
     init {
 
     }
     override fun periodic() {
-        SmartDashboard.putNumber("encoder", encoder.get().radians.asDegrees)
+        SmartDashboard.putNumber("encoder", encoder.rotations.asDegrees)
         if(Climb.climbLimitSwitch.get()) {
+            encoder.reset(0.0.radians)
             //encoder.offset -= encoder.rotations
         }
 
