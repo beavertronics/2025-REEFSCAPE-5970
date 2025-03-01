e053ba9f36f50009dcd891cea2298696d0773194
Sketched Doughnut <sketchedbusinessemail@gmail.com>
2025-03-01 13:35:06 -0800

    made basic auto to drive forwards for 1.5 seconds

---
+++ src/main/kotlin/frc/robot/commands/autos/driveForward.kt
@@ -0,0 +1,22 @@
+package frc.robot.commands.autos
+
+import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj2.command.Command
+import frc.robot.subsystems.DriveConstants
+import frc.robot.subsystems.Drivetrain
+
+object driveForward : Command() {
+    val timer = Timer()
+
+    init { addRequirements(Drivetrain) }
+
+    override fun initialize() { timer.restart() }
+
+    override fun execute() {
+        Drivetrain.rawDrive(0.1 * DriveConstants.MaxVoltage, 0.1 * DriveConstants.MaxVoltage)
+    }
+
+    override fun isFinished(): Boolean { return timer.hasElapsed(1.5) } // todo tune time
+
+    override fun end(interrupted: Boolean) { Drivetrain.stop() }
+}
