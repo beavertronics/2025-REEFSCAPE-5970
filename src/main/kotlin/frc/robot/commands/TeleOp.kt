671c0fddfe3c9eb5cf0da72a5f8740ec743ec46d
Will Kam <william@kamelot.me>
2025-03-10 18:03:01 -0700

    a

--- src/main/kotlin/frc/robot/commands/TeleOp.kt
+++ src/main/kotlin/frc/robot/commands/TeleOp.kt
@@ -1,13 +1,10 @@
 package frc.robot.commands
-import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj2.command.Command
 import kotlin.math.*
 
 import beaverlib.utils.Sugar.within
 import frc.robot.subsystems.Drivetrain
-import frc.robot.subsystems.Intake
-import frc.robot.subsystems.Shooter
 
 /*
 Controls the robot based off of inputs from the humans operating the driving station.
@@ -24,7 +21,7 @@ object TeleOp : Command() {
      * and that there isn't a time gap between things being called.
      */
     override fun initialize() {
-        addRequirements(Drivetrain) // todo add systems
+        addRequirements(Drivetrain)
     }
 
     /**
@@ -33,22 +30,15 @@ object TeleOp : Command() {
      * Executed very frame
      */
     override fun execute() {
-        //===== CHANGING STUFF =====//
-        var inverted = 1.0
-        if (OI.invertDirection) { inverted = -1.0 }
-        //===== DRIVETRAIN =====//
-        if (OI.toggleChildMode) {
-            Drivetrain.rawDrive(OI.childModeDriveLeft*3, OI.childModeDriveRight*3)
-        }
-        else {
-            Drivetrain.rawDrive(
-                (OI.parentModeDrive - OI.parentModeStraife) * 9,
-                (OI.parentModeDrive + OI.parentModeStraife) * 9)
-        }
+        Drivetrain.rawDrive(
+        (OI.forwards - OI.strafe) * 12,
+        (OI.forwards + OI.strafe) * 12
+        )
 
-        //===== SUBSYSTEMS =====//
-        Intake.runIntake(OI.runIntake*(1.0*inverted))
-        Shooter.runOpenLoop(OI.runShooter*(1.0*inverted))
+        print("forwards, strafe:")
+        print(OI.forwards)
+        print(", ")
+        println(OI.strafe)
     }
 
     /**
@@ -56,10 +46,7 @@ object TeleOp : Command() {
      * getting inputs from controllers and whatnot.
      */
     object OI {
-        private val operatorController = XboxController(2)
-        private val rightDrive = Joystick(0)
-        private val leftDrive = Joystick(1)
-
+        private val drivingController = XboxController(2)
 
         /**
          * Allows you to tweak controller inputs (ie get rid of deadzone, make input more sensitive by squaring or cubing it, etc).
@@ -78,24 +65,8 @@ object TeleOp : Command() {
             return this.absoluteValue > target
         }
 
-        val childModeDriveLeft get() = leftDrive.y.processInput()
-        val childModeDriveRight get() = rightDrive.y.processInput()
-        val parentModeDrive get() = operatorController.leftY.processInput()
-        val parentModeStraife get() = operatorController.leftX.processInput()
-        val runIntake get() = operatorController.leftTriggerAxis.processInput()
-        val runShooter get() = operatorController.rightTriggerAxis.processInput() * -1.0 // invert direction
-        val invertDirection get() = operatorController.rightBumperButton
-        val toggleChildMode get() = operatorController.leftBumperButton
-
-
-        /**
-         * Values for inputs go here
-         */
-        // todo
-//        val driveFieldOrientedForwards get() = drivingController.leftY.processInput()
-//        val driveFieldOrientedSideways get() = drivingController.leftX.processInput()
-//        val rotateRobot get() = drivingController.rightX.processInput()
-//        val toggleFieldOriented get() = drivingController.rightTriggerAxis
+        val forwards get() = drivingController.leftY.processInput() * -1
+        val strafe get() = drivingController.leftX.processInput()
     }
 }
 
